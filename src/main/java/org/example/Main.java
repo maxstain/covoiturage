ed49799044dba7182376dd52e84822a4cede6ce8
Firas Chabchoub <firaschabchoub@hotmail.com>
2024-11-29 21:38:08 +0100

    Working on the UserServices.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,12 +8,19 @@ public class Main {
     private static final UserController _userController = new UserController();
 
     public static void main(String[] args) {
-        User user = new User(_userController.getSize() + 1, "nom", "prenom", 20, "email", "motDePasse", "type");
+        User user = new User(_userController.getSize() + 1,
+                "Doe",
+                "John",
+                25,
+                "johndoe25@gmail.com",
+                "johndoe123",
+                "Conducteur"
+        );
+        _userController.truncate();
         _userController.ajout(user);
         _userController.afficher();
         user.setAge(30);
         _userController.modifier(user);
         _userController.afficher();
-        _userController.truncate();
     }
 }
