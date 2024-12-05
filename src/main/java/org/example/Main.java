68dc3137d9fb9c4ae2e90d543ccef844c482af25
Firas Chabchoub <firaschabchoub@hotmail.com>
2024-12-05 23:35:31 +0100

    Ajout la fonctionalité du cryptage du mot de passe.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,24 +4,32 @@ import org.example.Controllers.UserController;
 import org.example.DAO.User;
 import org.example.Utils.PasswordHash;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
 public class Main {
 
     private static final UserController _userController = new UserController();
 
     public static void main(String[] args) {
-        User user = new User(_userController.getSize() + 1,
-                "Chabchoub",
-                "Firas",
-                25,
-                "firaschabchoub@gmail.com",
-                PasswordHash.hashPassword("123456"),
-                "Admin"
-        );
-        _userController.truncate();
-        _userController.ajout(user);
-        _userController.afficher();
-        user.setAge(30);
-        _userController.modifier(user);
-        _userController.afficher();
+        User user = null;
+        try {
+            user = new User(_userController.getSize() + 1,
+                    "Chabchoub",
+                    "Firas",
+                    25,
+                    "firaschabchoub@gmail.com",
+                    PasswordHash.hashPassword("123456"),
+                    "Admin"
+            );
+            _userController.truncate();
+            _userController.ajout(user);
+            _userController.afficher();
+            user.setAge(30);
+            _userController.modifier(user);
+            _userController.afficher();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/org/example/Utils/PasswordHash.java
+++ src/main/java/org/example/Utils/PasswordHash.java
@@ -1,11 +1,28 @@
 package org.example.Utils;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
 public class PasswordHash {
-    public static String hashPassword(String mdp) {
-        return mdp.hashCode() + "";
+    static SecureRandom random = new SecureRandom();
+    static byte[] salt = new byte[16];
+
+    PasswordHash() {
     }
 
-    public static boolean checkPassword(String mdp, String hashedMdp) {
+    public static String hashPassword(String mdp) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        random.nextBytes(salt);
+        KeySpec spec = new PBEKeySpec(mdp.toCharArray(), salt, 65536, 128);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] hash = factory.generateSecret(spec).getEncoded();
+        return new String(hash);
+    }
+
+    public static boolean checkPassword(String mdp, String hashedMdp) throws NoSuchAlgorithmException, InvalidKeySpecException {
         return hashedMdp.equals(hashPassword(mdp));
     }
 }
